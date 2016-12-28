2d259c954f30701a9111f97cb3f3a328f8d3b079
Diego <diego@tecdata.net>
2016-12-28 20:24:53 -0300

    upgrade mod3_sol

--- mod3_solution/js/app.js
+++ mod3_solution/js/app.js
@@ -1,50 +1,86 @@
 (function(){
 	'use_strict';
 
-
 	angular.module('NarrowItDownApp',[])
 	.controller('NarrowItDownController',NarrowItDownCtrl)
 	.service('MenuSearchService',MenuSearchSvc)
-	.constant('MenuRestAPI','https://davids-restaurant.herokuapp.com/menu_items.json.')
+	.constant('MenuRestAPI','https://davids-restaurant.herokuapp.com/menu_items.json')
+	.filter('menu',MenuFilterFactory)
 	.directive('foundItems',FoundItemsDirective);
 
 	NarrowItDownCtrl.$inject = ['$scope','MenuSearchService'];
 	function NarrowItDownCtrl($scope,MenuSearchService){
-		var Searcher = this;
-		Searcher.searchTerm = "";		
-		Searcher.found = function(){
-			MenuSearchService.getMatchedMenuItems(Searcher.searchTerm);
-		}
-		
+		var searcher = this;
+		var service = MenuSearchService;
 
+		searcher.searchTerm = "";
+		searcher.found = function(){
+			service.getMatchedMenuItems(searcher.searchTerm)
+			.then(function(data){
+				if(typeof(data.status) != 'undefined'){
+					console.log("Error: ",data.status + " " + data.statusText);
+				}else{
+
+					console.log(data);
+				}
+			});
+		};
+
+		searcher.remove = function(index){
+			searcher.found.splice(index,1);
+		}
 	};
 
-	MenuSearchSvc.$inject = ['MenuRestAPI'];	
-	function MenuSearchSvc(MenuRestAPI){
-		var MenuSearch = this;
-		
-		MenuSearch.getMatchedMenuItems = function(searchTerm){
-			//Retrieve menu items. $http
-			//Make the search 
-			//Retrieve filtered list (promise)
-			//console.log("SearchTerm: ",searchTerm);
-			console.log([{item1:'elem1'},{item2:'elem2'}]);
+	MenuSearchSvc.$inject = ['MenuRestAPI','$http','menuFilter'];
+	function MenuSearchSvc(MenuRestAPI,$http,menuFilter){
+		var service = this;
+		var filter = menuFilter;
+
+		service.getMatchedMenuItems = function(searchTerm){
+			var promise = $http({
+				url:MenuRestAPI
+			}).then(function(response){
+					return filter(searchTerm,response.data.menu_items);
+				},function(response){
+					return response;
+				});
+			return promise;
+		};
+
+		return service;
+	}
+
+	function MenuFilterFactory(){
+		return function(searchTerm,items){
+			var result = [];
+			for(var i=0;i<items.length;i++){
+				var item = items[i];
+				if(item.description.indexOf(searchTerm) !== -1) result.push(item);
+			}
+			return result;
 		};
 	}
 
 	function FoundItemsDirective(){
 		var ddo = {
-			scope={
-				found: "<foundItems"
+			scope:{
+				foundItems: "=found"//,
+				//onRemove:"&onRemove"
 			},
+			// restrict: 'E',
 			controller: FoundItemsDirectiveController,
 			bindToController: true,
-			controllerAs: 'ctrl'
+			controllerAs: 'ctrl',
+			templateUrl: 'directives/FoundItems.html'
 		};
 		return ddo;
 	};
 
 	function FoundItemsDirectiveController(){
-		console.log(ctrl.found);
+		var ctrl = this;
+
+		ctrl.foundItemsFromIS = function(){
+			return ctrl.foundItems;
+		}
 	};
 })();
